f82545e3085c53e7312f9635aed1c41401e2e4fb
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2026-03-16 14:33:31 +0530

    Added user routes

--- routes/authRouter.go
+++ routes/authRouter.go
@@ -1,1 +1,11 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	controller "github.com/krityan/golang-jwt/controllers"
+)
+
+func AuthRoute(incomingRoute *gin.Engine) {
+	incomingRoute.POST("/user/signup", controller.SignUp());
+	incomingRoute.POST("/user/login", controller.Login());
+}
--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	middleware "github.com/krityan/golang-jwt/middlewares"
+	controller "github.com/krityan/golang-jwt/controllers"
+)
+
+func UserRoute(incomingRoute *gin.Engine) {
+	incomingRoute.Use(middleware.Authenticate());
+
+	incomingRoute.GET("/user", controller.GetUsers());
+
+	incomingRoute.GET("/user/:user_id", controller.GetUser());
+}
