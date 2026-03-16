38835d3f1246ccde6fb499a0df3c1fe29b53b059
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2026-03-16 15:14:07 +0530

    Added DBconnection

---
+++ config/dbConnection.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBinstance() *mongo.Client {
+	err := godotenv.Load(".env") // load the env variable
+
+	if err != nil {
+		log.Fatal("Error loading the env variable");
+	}
+
+	MongoDb:= os.Getenv("MONGO_URL");
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
+
+	if err != nil {
+		log.Fatal(err);
+	}
+  ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second); // WithTimeout returns WithDeadline(parent, time.Now().Add(timeout)).
+
+	defer cancel(); // it calls automatically after the function is executed
+
+	err = client.Connect(ctx);
+  if err != nil {
+		log.Fatal(err);
+	}
+
+	// database connection is established
+  fmt.Println("Database connection established successfully");
+
+	return client;
+
+}
+
+var Client *mongo.Client = DBinstance();
+
